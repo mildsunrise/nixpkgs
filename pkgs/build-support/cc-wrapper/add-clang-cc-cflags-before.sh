51ed057d29ac6f9cb01c8bdd86eeb48fc47690f0
Alba Mendez <me@alba.sh>
2025-03-16 23:28:58 +0100

    clang: drop local cflags only if a different target is passed
    12b0e8a/6f756b4 work around clang errors when a
    nix-wrapped compiler is used like a multi-target compiler.
    it does so by dropping the machineFlags whenever
    -target/--target is found in the arguments.
    
    however this breaks compiler-rt, which is designed to
    acommodate multi-target builds and always passes `--target`.
    in its packaging we make sure to only build it for the target
    we need, essentially disabling the multi-target aspect,
    but because it still passes --target, the machineFlags are
    dropped and compiler-rt could end up being for an invalid
    ABI, producing an unusable stdenv.cc.
    
    we could manually pass machineFlags to compiler-rt's
    cmake build, but IMO it makes more sense to tolerate
    --target arguments whose value coincides with the wrapper's,
    keeping the machineFlags in this case.

--- pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
+++ pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
@@ -1,15 +1,25 @@
-needsTarget=true
+needsLocalCflags=true
 
-for p in "${params[@]}"; do
+nParams=${#params[@]}
+declare -i n=0
+while (( "$n" < "$nParams" )); do
+    p=${params[n]}
+    p2=${params[n+1]:-} # handle `p` being last one
+    n+=1
+
     case "$p" in
-    -target | --target=*)
-        needsTarget=false
-
-        echo "Warning: supplying the --target argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
-        ;;
+        -target) overridenTarget="$p2"; n+=1 ;;
+        --target=*) overridenTarget="${p:9}" ;;
+        *) continue;
     esac
+
+    if [[ "$overridenTarget" != @defaultTarget@ ]]; then
+        echo "Warning: supplying a non-default --target argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
+
+        needsLocalCflags=false
+    fi
 done
 
-if $needsTarget && [[ $0 != *cpp ]]; then
+if $needsLocalCflags && [[ $0 != *cpp ]]; then
     extraBefore+=(-target @defaultTarget@ @machineFlags@)
 fi
